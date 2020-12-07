4feee1467c3ac0d93a5c0f43a7af6943c7d17081
AnstettVictor <anstett.victor67@gmail.com>
2020-12-07 15:13:44 +0100

    ajout vue recap panier + class

---
+++ laboutiquefrancaise/src/Controller/CartController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/mon-panier", name="cart")
+     */
+    public function index(Cart $cart, EntityManagerInterface $entityManager): Response
+    {
+        /**afficher le panier complet */
+        $cartComplete = [];
+
+        foreach ($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                'product' => $entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+    
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
+        ]);
+    }
+
+
+     /**
+     * @Route("/cart/add/{id}", name="add_to_cart")
+     */
+    public function add(Cart $cart, $id): Response
+    {
+
+        $cart->add($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+
+
+     /**
+     * @Route("/cart/remove", name="remove_my_cart")
+     */
+    public function remove(Cart $cart): Response
+    {
+
+        $cart->remove();
+
+        return $this->redirectToRoute('products');
+    }
+}
